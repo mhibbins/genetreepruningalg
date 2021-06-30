900f933b2138f06dac0bcc9ed08ccadf893aa314
Mark Hibbins <mhibbins@iu.edu>
2021-06-30 14:52:36 -0400

    Cleaned up read_arguments to fix segfault

--- src/main.cpp
+++ src/main.cpp
@@ -17,27 +17,7 @@ input_parameters read_arguments(int argc, char *const argv[])
         my_input_parameters.help = true;
         return my_input_parameters;
     }
-
-    int args; // getopt_long returns int or char
-    int prev_arg;
-
-    switch (args) {
-
-        case 'i':
-            my_input_parameters.input_file_path = optarg;
-            break;
-        //default: // '?' is parsed (
-        //    throw std::runtime_error(std::string("Unrecognized parameter: '") + (char)args + "'");
-            
-        }
-
-    //if (optind < argc)
-    //{
-    //    throw std::runtime_error(std::string("Unrecognized parameter: '") + argv[optind] + "'");
-    //}
-
-    //my_input_parameters.check_input(); // seeing if options are not mutually exclusive              
-
+    my_input_parameters.input_file_path = argv[1];
     return my_input_parameters;
 }
 
@@ -55,10 +35,10 @@ int main(int argc, char *const argv[]){
 
     //Print statements for debugging
     std::string sptree_string = clade_to_string(sptree);
-    std::string genetree_string = clade_to_string(sptree);
+    std::string genetree_string = clade_to_string(genetree);
     //std::cout << "If you see this, the code runs\n";
     //std::cout << sptree;
-    std::cout << sptree_string << genetree_string;
+    std::cout << sptree_string << std::endl << genetree_string << std::endl;
 
     return 0;
 }
