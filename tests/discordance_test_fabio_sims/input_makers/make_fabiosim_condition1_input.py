d35a538e74aa8b33f2edbdf968895aa51698bac9
Mark Hibbins <mhibbins@iu.edu>
2021-12-08 16:16:08 -0500

    Test using Fabio's math for simulation

---
+++ tests/discordance_test_fabio_sims/input_makers/make_fabiosim_condition1_input.py
@@ -0,0 +1,53 @@
+import sys 
+
+
+def read_traits(Routput):
+
+    traits = []
+ 
+    for line in Routput:
+        if len(line.split()) == 4: 
+        
+            split_traits = line.split()
+            sp4_trait, sp3_trait, sp2_trait = split_traits[1], split_traits[2], split_traits[3]
+            traits.append([sp4_trait, sp3_trait, sp2_trait])
+   
+
+    return traits
+
+def write_input(traits, file_prefix):
+
+    for i in range(len(traits)):
+        with open((file_prefix + str(i) + ".txt"), 'w') as inputfile:
+
+            inputfile.write("NEXUS\n")
+            inputfile.write("begin sptree;\n")
+            inputfile.write("(sp2:5.0,(sp3:0.4,sp4:0.4):4.6);\n")
+            inputfile.write("end;\n")
+            inputfile.write("\n")
+            inputfile.write("begin genetrees;\n")
+            inputfile.write("(sp2:6.0,(sp3:1.143,sp4:1.143):4.857);\n")
+            inputfile.write("(sp2:6.33,(sp3:5.33,sp4:5.33):1.0);\n")
+            inputfile.write("(sp3:6.33,(sp2:5.33,sp4:5.33):1.0);\n")
+            inputfile.write("(sp4:6.33,(sp2:5.33,sp3:5.33):1.0);\n")
+            inputfile.write("end;\n")
+            inputfile.write("\n")
+            inputfile.write("begin genetreefreqs;\n")
+            inputfile.write("0.899\n")
+            inputfile.write("0.033\n")
+            inputfile.write("0.033\n")
+            inputfile.write("0.033\n")
+            inputfile.write("end;\n")
+            inputfile.write("\n")
+            inputfile.write("begin traits;\n")
+            inputfile.write("set trait sp2 = " + str(traits[i][2]) + "\n")
+            inputfile.write("set trait sp3 = " + str(traits[i][1]) + "\n")
+            inputfile.write("set trait sp4 = " + str(traits[i][0]) + "\n")
+            inputfile.write("end;")
+
+
+
+Rout = sys.stdin
+traits = read_traits(Rout)
+write_input(traits, sys.argv[1])
+
---
+++ tests/discordance_test_fabio_sims/simulators/fabio_sim_condition4.R
@@ -0,0 +1,37 @@
+remove(list=ls())
+suppressMessages(suppressWarnings(library(tidyverse, warn.conflicts = F, quietly = T)))
+library(MASS, warn.conflicts = F, quietly = T)
+
+sim_BM_fabio <- function(t1, t2, N, n_traits) {
+  
+  
+  ### Theory ###
+  
+  tau = t2 - t1
+  scale = 2*N*0.00002 #assuming constant mu*simga2
+  
+  cov_AB <- (1 - exp(-tau/(2*N)))*(1 + 
+		(tau/(2*N) - (1 - (tau/(2*N))/(exp(tau/(2*N))-1)))) + 
+		(1/3)*exp(-tau/(2*N))
+
+  cov_nonsister <- (1/3)*exp(-tau/(2*N))
+
+  var = t1/10000 + (1 - exp(-tau/(2*N)))*(tau/(2*N) + 1) + 
+	  exp(-tau/(2*N))*(tau/(2*N) + 1 + 1/3)
+
+  var_covar <- scale*matrix(c(var, cov_AB, cov_nonsister,
+			      cov_AB, var, cov_nonsister,
+			      cov_nonsister, cov_nonsister, var),
+			    nrow = 3, ncol = 3)
+
+  
+  ### Trait value simulations ### 
+  
+  traits <- mvrnorm(n = n_traits, mu = c(0, 0, 0), Sigma = var_covar)
+  return(traits)
+  
+}
+
+test_traits <- sim_BM_fabio(4000, 50000, 50000, 100)
+  
+print(test_traits)
