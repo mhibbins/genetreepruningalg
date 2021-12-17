b2e93decfdcb19e37f210eb4c403febfdbd7bc76
Mark Hibbins <mhibbins@iu.edu>
2021-12-17 14:27:58 -0500

    Uncomment time slice test

--- src/main.cpp
+++ src/main.cpp
@@ -29,18 +29,18 @@ int main(int argc, char *const argv[]){
     double sigma2_initial = 0;
 
     //time slicer test
-    /*
+
     std::vector<int> genetree_nodes_preslice = count_nodes_all_trees(genetrees);
     for (int i = 0; i < genetree_nodes_preslice.size(); i++) {std::cout << genetree_nodes_preslice[i] << " ";}
     clade slice_test;
     slice_test.insert_between_all_trees(sptree, genetrees);
     std::vector<int> genetree_nodes_postslice = count_nodes_all_trees(genetrees);
-    for (int i = 0; i < genetree_nodes_postslice.size(); i++) {std::cout << genetree_nodes_postslice[i] << " ";}*/
+    for (int i = 0; i < genetree_nodes_postslice.size(); i++) {std::cout << genetree_nodes_postslice[i] << " ";}
 
-    std::unique_ptr<inference_optimizer_scorer> test_scorer(new sigma_optimizer_scorer(&sigma2_initial, sptree, genetrees, species_traits, genetreefreqs));
-    optimizer test_opt(test_scorer.get());
-    auto test_result = test_opt.optimize(user_input.optimizer_params); 
-    test_scorer->finalize(&test_result.values[0]);
+    //std::unique_ptr<inference_optimizer_scorer> test_scorer(new sigma_optimizer_scorer(&sigma2_initial, sptree, genetrees, species_traits, genetreefreqs));
+    //optimizer test_opt(test_scorer.get());
+    //auto test_result = test_opt.optimize(user_input.optimizer_params); 
+    //test_scorer->finalize(&test_result.values[0]);
 
     /*
     std::pair<double, double> sigma2_range(0.01, 2);
